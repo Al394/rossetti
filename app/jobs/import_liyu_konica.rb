b3788331008f2999627a788836f953a00232d3b8
Alessio Sangiorgi <sangiorgi.alessio94@gmail.com>
2023-03-31 14:55:18 +0200

    fix ImportJob

--- app/jobs/import_liyu_konica.rb
+++ app/jobs/import_liyu_konica.rb
@@ -1,5 +1,5 @@
 class ImportLiyuKonica < ApplicationJob
-  queue_as :demo
+  queue_as :rossetti
   sidekiq_options retry: 0, backtrace: 10
 
   def perform
--- app/jobs/import_versa_works.rb
+++ app/jobs/import_versa_works.rb
@@ -1,5 +1,5 @@
 class ImportVersaWorks < ApplicationJob
-  queue_as :demo
+  queue_as :rossetti
   sidekiq_options retry: 0, backtrace: 10
 
   def perform
